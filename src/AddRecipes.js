1ac3524c52416108c13f2eab1f48415f4a14d4f6
Amarnath0510 <amarmuthu007@gmail.com>
2021-12-29 19:32:45 +0530

    add recipes data added

---
+++ src/AddRecipes.js
@@ -0,0 +1,81 @@
+import { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+export function AddRecipes({ recipes, setRecipes }) {
+
+  const [name, setName] = useState("");
+  const [chef, setChef] = useState("");
+  const [cuisine, setCuisine] = useState("");
+  const [picture, setPicture] = useState("");
+  const [cooktimehours, setCooktimehours] = useState("");
+  const [ingredients, setIngredients] = useState("");
+  const [preparation, setPreparation] = useState("");
+  const addRecipe = () => {
+    console.log("Adding Recipes", name, picture, cuisine, chef, cooktimehours, ingredients, preparation);
+    const newRecipe = {
+      name,
+      chef,
+      cuisine,
+      picture,
+      cooktimehours,
+      ingredients,
+      preparation,
+    };
+    console.log(newRecipe);
+    setRecipes([...recipes, newRecipe]);
+  };
+  return (
+    <div>
+      <TextField
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+        id="standard-basic"
+        label="Enter a name"
+        variant="standard" />
+      <TextField
+
+        value={chef}
+        onChange={(event) => setChef(event.target.value)}
+        id="standard-basic"
+        label="Enter a chef"
+        variant="standard" />
+      <TextField
+
+        value={picture}
+        onChange={(event) => setPicture(event.target.value)}
+        id="standard-basic"
+        label="Enter a picture url"
+        variant="standard" />
+      <TextField
+
+        value={cuisine}
+        onChange={(event) => setCuisine(event.target.value)}
+        id="standard-basic"
+        label="Enter a cuisine"
+        variant="standard" />
+      <TextField
+
+        value={cooktimehours}
+        onChange={(event) => setCooktimehours(event.target.value)}
+        id="standard-basic"
+        label="Enter a  cooktimehours"
+        variant="standard" />
+      <TextField
+
+        value={ingredients}
+        onChange={(event) => setIngredients(event.target.value)}
+        id="standard-basic"
+        label="Enter a ingredients"
+        variant="standard" />
+      <TextField
+
+        value={preparation}
+        onChange={(event) => setPreparation(event.target.value)}
+        id="standard-basic"
+        label="Enter a Preparation method"
+        variant="standard" />
+      <Button onClick={addRecipe} variant="outlined">Add Recipe</Button>
+    </div>
+  );
+}
---
+++ src/Recipes.js
@@ -0,0 +1,23 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+
+export function Recipes({ name, chef, cuisine, picture, cooktimehours, ingredients, preparation }) {
+
+  return (
+    <section className="section-container">
+      <Card className="card-container" variant="outlined">
+        <CardContent>
+          <img className="img-container" src={picture} alt={name} />
+          <div className="sub-container">
+            <p>{name}</p>
+            <p>{chef}</p>
+            <p>{cuisine}</p>
+            <p>{cooktimehours}</p>
+            <p>{ingredients}</p>
+            <p>{preparation}</p>
+          </div>
+        </CardContent>
+      </Card>
+    </section>
+  );
+}
